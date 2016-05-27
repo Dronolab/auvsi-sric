14c30be559260a9cc25a67636cdebac0c3a41cde
Xeroxy <pierre.bonzel@gmail.com>
2016-05-27 16:34:36 -0400

    Port option added

--- updown.sh
+++ updown.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #updown.sh [IP] [TEAM DIR] [X DIR] [FTP USERNAME] [FTP PASSWORD] [FTP PORT] [LOCAL DIR]
-#ex : sudo sh updown.sh 192.168.0.106 Dronolab%20ETS Secret%20Folder%20Name pi club /Users/XX/Sites/test/
+#ex : sudo sh updown.sh 192.168.0.106 Dronolab%20ETS Secret%20Folder%20Name pi club 21 /Users/XX/Sites/test/
 
 HOST=$1
 TEAMDIR=$2
 XDIR=$3
 USERNAME=$4
 PASSWORD=$5
-
-LDIR=$6
+PORT=$6
+LDIR=$7
 FILEUP='upload.txt'
 FILEDOWN='download.txt'
 
 
-wget_output=$(wget -c --user=$USERNAME --password=$PASSWORD ftp://$HOST/$TEAMDIR/$XDIR/$FILEDOWN -o $LDIR/logDown -P $LDIR)
+wget_output=$(wget -c --user=$USERNAME --password=$PASSWORD ftp://$HOST:$PORT/$TEAMDIR/$XDIR/$FILEDOWN -o $LDIR/logDown -P $LDIR)
 if [ $? -ne 0 ]; then
 	echo "DOWNLOAD FAIL"
 else 
@@ -24,7 +24,7 @@ fi
 sleep 1
 
 cd $LDIR
-wput_output=$(wput -u $FILEUP ftp://$USERNAME:$PASSWORD@$HOST/$TEAMDIR/$XDIR/ >> logUp)
+wput_output=$(wput -u $FILEUP ftp://$USERNAME:$PASSWORD@$HOST:$PORT/$TEAMDIR/$XDIR/ >> logUp)
 if [ $? -ne 0 ]; then
 	echo "UPLOAD FAIL"
 else 
