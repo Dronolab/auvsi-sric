6cdff92dc75a8c79729d79f6f24e6f065afd7f71
Xeroxy <pierre.bonzel@gmail.com>
2016-06-06 08:57:51 -0400

    Update interfaces

--- sric.py
+++ sric.py
@@ -10,11 +10,11 @@ while wlan1ip == "":
 	time.sleep(1)
 	os.system("ifup wlan1")
 	status, wlan1ip = commands.getstatusoutput("ifconfig wlan1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'")
-	if wlan0ip == "":
+	if wlan1ip == "":
 		print "************ SRIC NOT CONNECTED ************"
 	else:
 		print "************ SRIC CONNECTED ************"
-		sric_status = commands.getstatusoutput("sudo sh updown.sh 192.168.0.106 auvsi/Dronolab%20ETS Secret%20Folder%20Name pi superclub 21 .")
+		sric_status = commands.getstatusoutput("sudo sh updown.sh 192.168.1.129 auvsi/Dronolab%20ETS Secret%20Folder%20Name pi superclub 21 .")
 		print sric_status
 
 except KeyboardInterrupt:
